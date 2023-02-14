a5113c48d37a921bafbdf4a338994fc470db8ee0
Lokesh Khandelwal <lokeshkhandelwal27@gmail.com>
2023-02-14 18:48:49 +0530

    updt

---
+++ Soft/AdvanceSallary.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Soft_AdvanceSallary : System.Web.UI.Page
+{
+    DataSet ds = new DataSet();
+    Master getdata = new Master();
+    Data data = new Data();
+    GetData Gd = new GetData();
+    private HttpCookie Soft;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Soft = Request.Cookies["STFP"];
+        if (!IsPostBack)
+        {
+            Gd.FillUser(drpEmployee);
+        }
+    }
+
+    protected void btnBack_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("AdvaneSallaryRep.aspx");
+    }
+
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("AdvaneSallaryRep.aspx");
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+
+    }
+}
